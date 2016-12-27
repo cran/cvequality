a195c88255e27b32e591552a6373b34869cf8edc
Ben Marwick <benmarwick@gmail.com>
2016-12-27 13:23:05 +0000

    version 0.1.1

---
+++ README.md
@@ -0,0 +1,19 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+[![Last-changedate](https://img.shields.io/badge/last%20change-2016--12--26-brightgreen.svg)](https://github.com/benmarwick/cvequality/commits/master) [![minimal R version](https://img.shields.io/badge/R%3E%3D-3.3.2-brightgreen.svg)](https://cran.r-project.org/) [![Licence](https://img.shields.io/github/license/mashape/apistatus.svg)](http://choosealicense.com/licenses/mit/) [![Travis-CI Build Status](https://travis-ci.org/benmarwick/cvequality.svg?branch=master)](https://travis-ci.org/benmarwick/cvequality) [![Coverage Status](https://img.shields.io/codecov/c/github/benmarwick/cvequality/master.svg)](https://codecov.io/github/benmarwick/cvequality?branch=master) [![ORCiD](https://img.shields.io/badge/ORCiD-0000--0001--7879--4531-green.svg)](http://orcid.org/0000-0001-7879-4531)
+
+cvequality: A package of tests for the equality of coefficients of variation from multiple groups
+-------------------------------------------------------------------------------------------------
+
+This package contains functions to test for significant differences in coefficients of variation (CVs) among multiple groups of observations.
+
+To install the package:
+
+``` r
+if (!require("ghit")) install.packages("ghit") # check if you have ghit (to install pkgs from github), if not, it will be installed here
+ghit::install_github("benmarwick/cvequality")
+```
+
+Please see the [vignette](vignettes/how_to_test_CVs.Rmd) for detailed examples of how to use the functions in this package.
+
+Please note that this project is released with a [Contributor Code of Conduct](CONDUCT.md). By participating in this project you agree to abide by its terms.
---
+++ inst/doc/how_to_test_CVs.R
@@ -0,0 +1,147 @@
+## ----setup, include=FALSE------------------------------------------------
+knitr::opts_chunk$set(echo = TRUE,
+                      warning = FALSE,
+                      message = FALSE)
+
+## ----eval = FALSE--------------------------------------------------------
+#  # we use the devtools package to install from GitHub
+#  # you may need to run install.packages("devtools") first
+#  devtools::install_github("benmarwick/cvequality")
+
+## ------------------------------------------------------------------------
+# read in the data, we obtained this from the HistData package
+GaltonFamilies <- read.csv("GaltonFamilies.csv")
+
+library(knitr)
+kable(head(GaltonFamilies), caption = "Preview of first few rows of the GaltonFamilies data")
+
+## ------------------------------------------------------------------------
+library(ggplot2)
+library(ggbeeswarm)
+ggplot(GaltonFamilies, 
+       aes(gender, 
+           childHeight)) +
+  geom_boxplot() +
+  geom_quasirandom(alpha = 0.05) +
+  theme_bw()
+
+## ------------------------------------------------------------------------
+# test for difference in CVs between boys and girls
+library(cvequality)
+child_height_by_gender_cv_test <- 
+with(GaltonFamilies, 
+     asymptotic_test(childHeight,
+                     gender))
+
+## ------------------------------------------------------------------------
+
+child_height_by_gender_cv_test_MSLRT <- 
+with(GaltonFamilies, 
+     mslr_test(nr = 1e4, 
+               childHeight,
+               gender))
+
+## ---- echo = FALSE-------------------------------------------------------
+# make a table to display the results
+child_test_summary <- 
+  data.frame(`Test name` = c("asymptotic", 
+                           "M-SLRT"),
+             `Test statistic` = c(child_height_by_gender_cv_test$D_AD,
+                                  child_height_by_gender_cv_test_MSLRT$MLRT),
+             `p-value` = c(child_height_by_gender_cv_test$p_value,
+                                  child_height_by_gender_cv_test_MSLRT$p_value),
+             check.names = FALSE)
+
+kable(child_test_summary, 
+      caption =  "Summary of tests on the equality of CVs in child heights by gender")
+
+## ------------------------------------------------------------------------
+# read in the data, we obtained this from the archdata package
+handaxes <- read.csv("Handaxes.csv")
+
+# take a quick look
+kable(head(handaxes), 
+      caption = "Preview of first few rows of the handaxes data")
+
+## ------------------------------------------------------------------------
+library(tidyverse)
+handaxes_reshape <- 
+  handaxes %>% 
+  select(c(L, L1, B, B1, B2, T, T1)) %>% 
+  gather(variable, value) %>% 
+  na.omit
+
+# take a quick look
+kable(head(handaxes_reshape), 
+      caption = "Preview of first few rows of the handaxes data")
+
+## ------------------------------------------------------------------------
+ggplot(handaxes_reshape, 
+       aes(variable, 
+           value)) +
+  geom_boxplot() +
+  geom_quasirandom(alpha = 0.02) +
+  theme_bw()
+
+## ------------------------------------------------------------------------
+handaxes_asymptotic_test <- 
+with(handaxes_reshape, 
+     asymptotic_test(value, 
+                     variable))
+
+handaxes_mlrt_test <- 
+with(handaxes_reshape, 
+     mslr_test(nr = 1e4, 
+               value, 
+               variable))
+
+## ---- echo = FALSE-------------------------------------------------------
+# make a table to display the results
+handaxe_test_summary <- 
+  data.frame(`Test name` = c("asymptotic", 
+                           "M-SLRT"),
+             `Test statistic` = c(handaxes_asymptotic_test$D_AD,
+                                  handaxes_mlrt_test$MLRT),
+             `p-value` = c(handaxes_asymptotic_test$p_value,
+                           handaxes_mlrt_test$p_value),
+             check.names = FALSE)
+
+kable(handaxe_test_summary, caption =  "Summary of tests on the equality of CVs in measurements of different variables of handaxe size")
+
+## ------------------------------------------------------------------------
+miller <- data.frame(test = c('ELISA', 'WEHI', '`Viral inhibition`'),
+                     Mean = c(6.8, 8.5, 6.0),
+                     CV =   c(0.090, 0.462, 0.340),
+                     N =    c(5, 5, 5))
+
+## ------------------------------------------------------------------------
+# compute SD from mean and cv
+miller$SD <- with(miller, CV * Mean)
+
+## ------------------------------------------------------------------------
+ggplot(miller,
+       aes(test,
+           Mean)) +
+  # points to show mean values
+  geom_point(size = 4) +
+  # lines to show standard deviations
+  geom_linerange(aes(ymin = Mean - SD,
+                     ymax = Mean + SD)) +
+  theme_bw()
+
+## ------------------------------------------------------------------------
+miller_asymptotic_test2 <- 
+asymptotic_test2(k = nrow(miller), 
+                 n = miller$N, 
+                 s = miller$SD, 
+                 x = miller$Mean)
+
+
+## ------------------------------------------------------------------------
+miller_mlrt_test2 <-
+mslr_test2(nr = 1e4, 
+           n = miller$N, 
+           s = miller$SD, 
+           x = miller$Mean)
+
+
